ae1b1c3b0829625f95de1f7124d74ae793e0ee16
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-04-24 01:52:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count a word:"
+    }
+]);
+const words = answers.Sentence.trim().split(" "); // "Sentence" ko lowercase "sentence" mein badal diya gaya hai.
+//print the array of words to the console
+console.log(words);
+// print the word count of the sentence
+console.log(`Your sentence word count is ${words.length}`);
